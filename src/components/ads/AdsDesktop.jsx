81a2fa4c8804f4606b76db74d7c34d8321f40647
iromanos <alessandroromano.net@gmail.com>
2026-06-30 16:45:00 +0200

    -

--- src/components/ads/AdsDesktop.jsx
+++ src/components/ads/AdsDesktop.jsx
@@ -3,19 +3,26 @@ import React from "react";
 import useMobile from "@/hooks/useMobile";
 import useInteraction from "@/hooks/useInteraction";
 
-export function AdsDesktop({children, className}) {
+export function AdsDesktop({children, className, height = 96}) {
 
     const {active} = useInteraction();
 
+    // const [active, setActive] = useState(false);
+
     const {isMobile} = useMobile();
 
-    return <div className={className}>
+    return <div className={className}
+                style={{
+                    height: `${height}px`
+                }}>
 
         {active === false &&
-            <div className={'bg-secondary-subtle d-none d-lg-flex justify-content-center align-items-center'}
-                 style={{
-                     height: '82px'
-                 }}><span>PUBBLICITA'</span></div>
+            <div
+                onClick={() => {
+                    // setActive(true);
+                }}
+                className={'bg-secondary-subtle d-none d-lg-flex justify-content-center align-items-center h-100'}
+            ><span>PUBBLICITA'</span></div>
         }
 
         {active && isMobile === false && children}
--- src/components/ads/Display-6977770298.jsx
+++ src/components/ads/Display-6977770298.jsx
@@ -4,24 +4,20 @@ import {useEffect} from "react";
 import {AdSenseDev} from "./LoadAdSense";
 
 
-export default function Display6977770298({className}) {
+export default function Display6977770298({className, height = 96}) {
 
     useEffect(() => {
-        // console.log("CHECK ADSENSE");
         try {
             (window.adsbygoogle = window.adsbygoogle || []).push({});
         } catch (e) {
-            // console.error("AdSense error:", e);
         }
     }, []);
 
-    return <>
-        <ins className={'adsbygoogle ' + className}
-             style={{display: 'inline-block', height: '82px', width: '100%'}}
+    return <div className={className} style={{height: `${height}px`}}>
+        <ins className={'adsbygoogle '}
+             style={{display: 'inline-block', height: `${height}px`, width: '100%'}}
              data-ad-client="ca-pub-7775238513283854"
              data-ad-slot="6977770298"
-        ><AdSenseDev style={{
-            height: '82px'
-        }} slotAdSense={"6977770298"}/></ins>
-    </>;
+        ><AdSenseDev slotAdSense={"6977770298"}/></ins>
+    </div>;
 }
