98edb94cfa4cfda1b893066c6c83d95f81fed7c2
iromanos <alessandroromano.net@gmail.com>
2026-07-23 15:48:16 +0200

    -

--- src/repos/mysql.js
+++ src/repos/mysql.js
@@ -1,25 +1,33 @@
 import mysql from 'mysql2/promise';
 
 export async function connectToDatabase() {
-
+    return createPool();
+    /*
     return mysql.createConnection({
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-    });
+    });*/
 }
 
 export function createPool() {
-    return mysql.createPool({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        waitForConnections: true,
-        connectionLimit: 20, // Adjust based on your server capacity
-        queueLimit: 0
-    });
+
+    if (!global._mysqlPool) {
+
+        global._mysqlPool = mysql.createPool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            waitForConnections: true,
+            connectionLimit: 50,
+            queueLimit: 0
+        });
+    }
+
+    return global._mysqlPool;
+
 }
